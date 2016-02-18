fd9a2e15d09701c75651b91e10ad1cc5545a8968
Leandro Henrique Reis <emtudo@gmail.com>
2016-02-18 21:42:44 -0200

    max length

--- resources/views/creditcard/new.blade.php
+++ resources/views/creditcard/new.blade.php
@@ -16,8 +16,8 @@
                     <tbody>
                             <tr>
                                 <td><input class="form-control" type="text" v-model="creditcard.number" placeholder="Number"></td>
-                                <td><input class="form-control" type="text" v-model="creditcard.valid" placeholder="Valid MM/YY"></td>
-                                <td><input class="form-control" type="text" v-model="creditcard.cvv" placeholder="CVV"></td>
+                                <td><input class="form-control" type="text" v-model="creditcard.valid" placeholder="Valid MM/YY" maxlength="5"></td>
+                                <td><input class="form-control" type="text" v-model="creditcard.cvv" placeholder="CVV" maxlength="4"></td>
                                 <td><input class="form-control" type="text" v-model="creditcard.password" placeholder="Password"></td>
                                 <td><input class="form-control" type="text" v-model="creditcard.data_crypt" placeholder="Data"></td>
                                 <td><input class="form-control" type="text" v-model="creditcard.note" placeholder="Note"></td>
